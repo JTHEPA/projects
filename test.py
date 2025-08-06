9b914742bfb6705c1eec97dc5b8ed8cc19228bd6
joseph thepa <josephmafedi@gmail.com>
2025-08-06 10:09:12 +0200

    initial commit

--- test.py
+++
@@ -1,8 +0,0 @@
-def life():
-    user = int(input("how old are you? "))
-    if user < 18:
-        return print("you are too young")
-    else:
-        return print("you are young")
-    return user
-life()
